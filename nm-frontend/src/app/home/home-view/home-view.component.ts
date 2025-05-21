ee0518124eac8fc96f710f57b3f52de6202fb524
Ignacy Smoliński <ignacy.smolinski@gmail.com>
2025-05-21 16:32:00 +0200

    feat: home-view default

--- nm-frontend/src/app/home/home-view/home-view.component.ts
+++ nm-frontend/src/app/home/home-view/home-view.component.ts
@@ -1,7 +1,7 @@
 import {Component, inject, OnDestroy, OnInit} from '@angular/core';
 import {HomeLoaderService, UpdatesState} from '../home-loader.service';
 import {Subscription} from 'rxjs';
-import {Router, RouterOutlet} from '@angular/router';
+import {ActivatedRoute, Router, RouterOutlet} from '@angular/router';
 import {UpdatePreviewBoxComponent} from '../update-preview-box/update-preview-box.component';
 
 @Component({
@@ -21,6 +21,8 @@ export class HomeViewComponent implements OnInit, OnDestroy {
   private updatesSubscription!: Subscription;
   protected displayedUpdates: UpdatesState | null = null;
 
+  protected isDefaultView = true;
+
   ngOnInit(): void {
     if (!this.homeLoaderService.isLoaded()) this.homeLoaderService.startFetchingUpdates();
     this.updatesSubscription = this.homeLoaderService
@@ -28,9 +30,10 @@ export class HomeViewComponent implements OnInit, OnDestroy {
       .subscribe((fetchingResult: UpdatesState | null) => {
         this.displayedUpdates = fetchingResult;
       });
+    this.isDefaultView = this.activatedRoute.children.length === 0;
   }
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
   }
 
   ngOnDestroy(): void {
