509b11bf553513f7fa7ba2274527c0aef3edb0e1
Ignacy Smoliński <ignacy.smolinski@gmail.com>
2025-05-23 15:27:11 +0200

    feat: site register UI

--- nm-frontend/src/app/home/utils/utils.ts
+++ nm-frontend/src/app/home/utils/utils.ts
@@ -1,5 +1,19 @@
+import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
+
 export function getStatusClass(statusCode: number): string {
   if(statusCode < 400) return 'successU';
   else if(statusCode < 500) return 'clientEU';
   else return 'serverEU';
+}
+
+export function urlValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const urlPattern = /^(https?:\/\/)?([\w-]+(\.[\w-]+)+)([\/?]?.*)$/i;
+    const value: string | null | undefined = control.value;
+    if (typeof value !== 'string' || value.length === 0) {
+      return null;
+    }
+    const isValid = urlPattern.test(value);
+    return isValid ? null : { invalidUrl: true };
+  };
 }
