6211d63c9a69e7d7ffc875e736e97cbc422c6eae
Aleksander <141730118+janicaleksander@users.noreply.github.com>
2025-06-04 10:51:26 +0200

    Repair of observers management

--- src/backend/django_app/tracking/urls.py
+++ src/backend/django_app/tracking/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import SiteView,elementId,TrackingElementChangeView,LastKUpdatesView,SearchSuggestionView, ObserverView,GotifyView
+from .views import SiteView,elementId,TrackingElementChangeView,LastKUpdatesView,SearchSuggestionView, GotifyView
 urlpatterns = [
     path("site/",SiteView.as_view(),name="site_view"),
     path("element/",TrackingElementChangeView.as_view(),name="site_view"),
     path("element-id/",elementId.as_view(),name="element_view"),
     path("updates/",LastKUpdatesView.as_view(),name="last_update_view"),
     path("search/",SearchSuggestionView.as_view(),name="search_view"),
-    path("observer/",ObserverView.as_view(),name="observer_view"),
     path("gotify/",GotifyView.as_view(),name="gotify_view"),
 
 
